fe0232d4cc0ee4a193fc946ae0f9513306f39e65
Guilherme <guilhaugusto05@gmail.com>
2022-03-30 20:45:09 -0300

    Fix connection file adding database and removing port variable

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
 
-// ps: environment variables must be defined in a .env file
+// ps: environment variables must be defined in an .env file
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  database: 'StoreManager',
 });
 
 module.exports = connection;
