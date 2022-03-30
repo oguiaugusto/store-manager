273029ce8c1ea4fce803e78a326e031a39804459
Guilherme <guilhaugusto05@gmail.com>
2022-03-30 16:04:18 -0300

    Create productsModel and tests for it

---
+++ models/productsModel.js
@@ -0,0 +1,23 @@
+const connection = require('./connection');
+
+const listAll = async () => {
+  const [products] = await connection.execute('SELECT * FROM StoreManager.products');
+  
+  if (!products || products.length === 0) return null;
+  return products;
+};
+
+const listById = async (id) => {
+  const [product] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?',
+    [id],
+  );
+
+  if (!product || product.length === 0) return null;
+  return product;
+};
+
+module.exports = {
+  listAll,
+  listById,
+};
---
+++ test/unit/models/productsModel.js
@@ -0,0 +1,102 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../models/connection');
+const productsModel = require('../../../models/productsModel');
+
+const singleProduct = {
+  id: 1,
+  name: 'Product1',
+  quantity: 1,
+};
+
+const arrayOfProducts = [
+  { id: 1, name: 'Product1', quantity: 1 },
+  { id: 2, name: 'Product2', quantity: 2 },
+  { id: 3, name: 'Product3', quantity: 3 },
+];
+
+const ID_TEST = 1;
+
+describe('productsModel.js', () => {
+  describe('listAll should', () => {
+    describe('when no product is found: ', () => {
+      before(async () => {
+        const execute = [];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return null', async () => {
+        const response = await productsModel.listAll();
+        expect(response).to.be.null;
+      });
+    });
+
+    describe('when products are found', () => {
+      before(async () => {
+        const execute = [arrayOfProducts];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return an array of objects', async () => {
+        const response = await productsModel.listAll();
+
+        expect(response).to.be.an('array');
+        response.forEach((item) => expect(item).to.be.an('object'));
+      });
+      it('return an array with the expected values', async () => {
+        const response = await productsModel.listAll();
+
+        expect(response).to.have.deep.members(arrayOfProducts);
+      });
+      it('the objects has expected keys and values', async () => {
+        const response = await productsModel.listAll();
+
+        expect(response[0]).to.have.property('id');
+        expect(response[0]).to.have.property('name');
+        expect(response[0]).to.have.property('quantity');
+
+        expect(response[0]).to.be.eql(arrayOfProducts[0]);
+      });
+    });
+  });
+
+  describe('listById should', () => {
+    describe('when no product is found', () => {
+      before(async () => {
+        const execute = [[]];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return null', async () => {
+        const response = await productsModel.listById(ID_TEST);
+        expect(response).to.be.null;
+      });
+    });
+
+    describe('when the product is found', () => {
+      before(async () => {
+        const execute = [singleProduct];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return an object', async () => {
+        const response = await productsModel.listById(ID_TEST);
+        expect(response).to.be.an('object');
+      });
+      it('the object has expected keys and values', async () => {
+        const response = await productsModel.listById(ID_TEST);
+
+        expect(response).to.have.property('id');
+        expect(response).to.have.property('name');
+        expect(response).to.have.property('quantity');
+
+        expect(response).to.be.eql(singleProduct)
+      });
+    });
+  });
+});
