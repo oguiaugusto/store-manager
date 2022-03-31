4d0f6461489d089f0b6a2c3d66014cb1971c7d45
Guilherme <guilhaugusto05@gmail.com>
2022-03-31 16:02:06 -0300

    Create routes post, put and delete to /products (and /:id)

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const productsController = require('../controllers/productsController');
+const validations = require('../middlewares/productsValidations');
 
 const router = express.Router();
 
 router.get('/', productsController.listAll);
 router.get('/:id', productsController.listById);
+
+router.post('/', validations, productsController.create);
+router.put('/:id', validations, productsController.update);
+router.delete('/:id', productsController.remove);
 
 module.exports = router;
